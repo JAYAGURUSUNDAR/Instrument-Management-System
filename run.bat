31c24ab392f1e47edf7227e1287f815f6c7995a4
JAYAGURUSUNDAR <72074531+JAYAGURUSUNDAR@users.noreply.github.com>
2023-10-13 22:52:49 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,28 @@
 @echo off
 setlocal
-python pyinstaller main.py --onefile --name IMS
+
+:: Check if Python is installed
+python --version 2>NUL
+if %errorlevel% NEQ 0 (
+    echo Python is not installed. Installing Python...
+    
+    :: Download and install Python
+    :: Change the URL to the latest Python installer URL
+    powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.x.x/python-3.x.x-amd64.exe', 'python_installer.exe')"
+    
+    :: Install Python silently
+    python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
+
+    :: Clean up the installer (optional)
+    del python_installer.exe
+)
+
+:: Install PyInstaller using pip
+python -m pip install pyinstaller
+
+:: Convert your Python script to an executable
+pyinstaller --onefile your_script.py
+
+:: Replace "your_script.py" with the actual name of your Python script
+
 endlocal
