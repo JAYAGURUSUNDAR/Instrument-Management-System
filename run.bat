6a6216364ee299188d76247096a28b7420a97f00
JAYAGURUSUNDAR <72074531+JAYAGURUSUNDAR@users.noreply.github.com>
2023-10-14 11:14:37 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,30 +1,17 @@
 @echo off
 setlocal
 
-:: Check if Python is installed
 python --version 2>NUL
 if %errorlevel% NEQ 0 (
-    echo Python is not installed. Installing Python...
-    
-    :: Download and install Python
-    :: Change the URL to the latest Python installer URL
+    echo Python is not installed. Installing Python..
     powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.x.x/python-3.x.x-amd64.exe', 'python_installer.exe')"
-    
-    :: Install Python silently
-    python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
-
-    :: Clean up the installer (optional)
+    python_installer.exe /quiet InstallAllUsers=1 PrependPath=
     del python_installer.exe
 )
 
-:: Install PyInstaller using pip
 python -m pip install pyinstaller
 
-:: Convert your Python script to an executable with a custom icon
 pyinstaller --onefile --noconsole --icon=IMS.ico main.py --name IMS
-
-:: Replace "main.py" with the actual name of your Python script
-:: Replace "app_icon.ico" with the actual path to your custom icon
 
 :: Create a desktop shortcut
 set SHORTCUT_PATH=%USERPROFILE%\Desktop\IMS.lnk
